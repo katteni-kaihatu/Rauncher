131ab7f6eee983ff81a0a0200174ad0c8b265d5e
rassi0429 <taitokokoa+rassi@gmail.com>
2023-11-08 01:08:29 +0900

    fix: splash load

--- electron/window/splash.ts
+++ electron/window/splash.ts
@@ -1,14 +1,15 @@
 import { app, BrowserWindow} from 'electron'
 import {join} from "path";
+import path from "node:path";
 
 process.env.DIST_ELECTRON = join(__dirname, '..')
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
 process.env.PUBLIC = app.isPackaged ? process.env.DIST : join(process.env.DIST_ELECTRON, '../public')
 
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
 const preload = join(__dirname, '../dist-electron/preload.js')
 const url = process.env.VITE_DEV_SERVER_URL
-const indexHtml = join(process.env.DIST, 'index.html')
 
 
 export class SplashWindow {
@@ -26,10 +27,10 @@ export class SplashWindow {
         contextIsolation: true,
       },
     })
-    if (process.env.VITE_DEV_SERVER_URL) {
+    if (VITE_DEV_SERVER_URL) {
       this.window.loadURL(`${url}?splash=true`)
     } else {
-      this.window.loadFile(indexHtml, { query: {'splash': 'true'} })
+      this.window.loadFile(path.join(process.env.DIST, 'index.html'), { query: {'splash': 'true'} })
     }
   }
 }
